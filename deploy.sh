bd51540404ae398ebd880d46b808ebd2a4f6fdf7
eopche <tniasu@qq.com>
2024-09-17 16:41:14 +0800

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -rf build/
+rm -rf output/
+forester build forest.toml
+# Since firefox doesn't want to work
+# ./compile-katex.sh
+
+cd ../forest-deploy
+git rm -rf .
+git checkout HEAD -- .gitignore
+git clean -fxd
+
+cd ../forest
+cp -a ./output/. ../forest-deploy
+
+# cd ../forest-deploy
+git add .
+git commit -m "Deploy"
+git push
