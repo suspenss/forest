c4e1c352fb12506af23f9039e2a7d7d3c5cf9d9c
eopche <tniasu@qq.com>
2024-10-16 20:38:58 +0800

    add flake

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs, system, forester, ... }:
+
+pkgs.mkShell {
+  packages =
+    (with pkgs; [
+      texlive.combined.scheme-full
+      fswatch
+
+      yamlfmt
+    ]) ++ [
+      forester.packages.${system}.default
+    ];
+}
